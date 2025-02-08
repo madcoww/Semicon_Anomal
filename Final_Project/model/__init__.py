d9ce96ad28d4029427f18ff301bf384da81d71bf
madcoww <74662313+madcoww@users.noreply.github.com>
2025-02-08 18:48:08 +0900

    Delete Final_Project directory

--- Final_Project/model/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Author : Wonjun Kim
-e-mail : wonjun.kim@seculayer.com
-Powered by Seculayer © 2024 AI Team, R&D Center.
-"""
-def model():
-    return None
--- Final_Project/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PYTHON_EXEC="/SSD/ai_test/.venv/bin/python3.11"
-SCRIPT_NAME="/SSD/ai_test/AlbertMain.py"
-
-nohup "$PYTHON_EXEC" "$SCRIPT_NAME" > output.log 2>&1 &
-echo $! > pid.file
-echo "Script is running in the background with PID $(cat pid.file)"
